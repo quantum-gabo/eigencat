bed00e414bc15f6d1ddfce873325135b1f6b7e6f
quantum-gabo <gabrielbala2001@gmail.com>
2026-02-25 00:02:59 -0500

    Update astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://eigencat.blog/",
-	base: "/",
+	site: "https://quantum-gabo.github.io",
+	base: "/eigencat",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
